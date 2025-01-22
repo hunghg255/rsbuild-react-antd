c38ea138d6c74976bf729fcaf06142900c2a8004
hunghg255 <giahunghust@gmail.com>
2025-01-22 16:38:13 +0700

    chore: update

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, type ErrorInfo, type ReactNode } from 'react'
+import React, { Component, type ErrorInfo, type ReactNode } from 'react';
 
 interface Props {
   children?: ReactNode;
@@ -11,15 +11,15 @@ interface State {
 class ErrorBoundary extends Component<Props, State> {
   public state: State = {
     hasError: false,
-  }
+  };
 
   public static getDerivedStateFromError(): State {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true }
+    return { hasError: true };
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error('Uncaught error:', error, errorInfo)
+    console.error('Uncaught error:', error, errorInfo);
   }
 
   public render() {
@@ -28,11 +28,11 @@ class ErrorBoundary extends Component<Props, State> {
         <h1>
           Sorry.. there was an error
         </h1>
-      )
+      );
     }
 
-    return this.props.children
+    return this.props.children;
   }
 }
 
-export default ErrorBoundary
+export default ErrorBoundary;
